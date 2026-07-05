d6c3727aee575c5865bdae3c9279981921107b73
OJOTIMILOLA <dayote124@gmail.com>
2026-07-05 21:21:01 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+print("CALCULATOR")
+print("")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+match operator:
+    case "+":
+        print("Answer =", num1 + num2)
+
+    case "-":
+        print("Answer =", num1 - num2)
+
+    case "*":
+        print("Answer =", num1 * num2)
+
+    case "/":
+        if num2 != 0:
+            print("Answer =", num1 / num2)
+        else:
+            print("Error! Cannot divide by zero.")
+
+    case _:
+        print("Invalid operator!")
